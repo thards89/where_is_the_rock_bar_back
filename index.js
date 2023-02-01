e22c92d0134fc8fd5db8ba8df7475b9e53eea438
thards89 <thards89@gmail.com>
2023-02-01 23:02:47 +0100

    cors updated

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ const { PORT } = require("./config/constants");
 // Create an express app
 const app = express();
 
-/**
- * Middlewares
- *
- * It is advisable to configure your middleware before configuring the routes
- * If you configure routes before the middleware, these routes will not use them
- *
- */
-
-// CORS middleware:  * Since our api is hosted on a different domain than our client
-// we are are doing "Cross Origin Resource Sharing" (cors)
-// Cross origin resource sharing is disabled by express by default
 const corsOptions = {   
   origin: "*",   
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",   
